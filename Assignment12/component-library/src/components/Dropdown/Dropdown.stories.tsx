5f6decb0bdd1e167e0a05b81b96285fe698ab368
zoeyChenCap <capchen113@163.com>
2025-06-30 02:44:20 -0500

    fix the import error

--- Assignment12/component-library/src/components/Dropdown/Dropdown.stories.tsx
+++ Assignment12/component-library/src/components/Dropdown/Dropdown.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { Dropdown } from './Dropdown';
 
 const meta: Meta<typeof Dropdown> = {
--- Assignment12/component-library/src/components/Img/Img.stories.tsx
+++ Assignment12/component-library/src/components/Img/Img.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { Img } from './Img';
 
 const meta: Meta<typeof Img> = {
